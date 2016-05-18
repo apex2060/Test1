710c30bf9bb1caea4a257984ee42a3add2e92b25
Ryan Quinlan <apex2060@site4u2.com>
2016-05-18 22:03:14 +0000

    custom init config

--- app/config.js
+++ app/config.js
@@ -1,16 +1,16 @@
 app.factory('config', function ($http, $q) {
 	var config = {};
 	var rootConfig = {
-		appsDomain: 	'easybusiness.center',
-		secureUrl: 		'https://the.easybusiness.center',
-		oauth: 			encodeURI('https://the.easybusiness.center/oauth'),
+		appsDomain: 	'test.domain',
+		secureUrl: 		'https://the.test.center',
+		oauth: 			encodeURI('https://the.test.domain/oauth'),
 		parse: {
 			root: 		'https://api.parse.com/1',
 			appId: 		'ETf61cYOebIkncxvVgrldjmPX4Z2acpWiKfY9wWM',
 			jsKey: 		'i3MNq4GYuP6ays3LNQdijimLuaN5uOJst1n87bVy',
 			restKey: 	'Wcpk6SaGnzklz5S0OhtngeYD6KJzNIoQ3VmyUgtK'
 		},
-		firebase: 		'https://easybusiness.firebaseio.com',
+		firebase: 		'https://test.firebaseio.com',
 		nest: 			'7e57b122-f5db-4807-b83e-642a9f5ba667',
 		plaid: {
 			client_id: 	'54b57cb9d6c6ee9c0a5d4286',
@@ -18,12 +18,12 @@ app.factory('config', function ($http, $q) {
 		},
 		google: {
 			"client_id": "821954483-q3ooncrbh8cmo8ukcupov77hfn41i6g9.apps.googleusercontent.com",
-			"project_id": "easy-business-center",
+			"project_id": "easy-business-domain",
 			"auth_uri": "https://accounts.google.com/o/oauth2/auth",
 			"token_uri": "https://accounts.google.com/o/oauth2/token",
 			"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-			"redirect_uris": ["https://root-apex2060.c9users.io/oauth", "https://easybusiness.center/oauth", "http://easybusiness.center/oauth"],
-			"javascript_origins": ["https://root-apex2060.c9users.io", "http://easybusiness.center", "https://easybusiness.center"],
+			"redirect_uris": ["https://root-apex2060.c9users.io/oauth", "https://test.domain/oauth", "http://test.domain/oauth"],
+			"javascript_origins": ["https://root-apex2060.c9users.io", "http://test.domain", "https://test.domain"],
 		},
 	}
 	
